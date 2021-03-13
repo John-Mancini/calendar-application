d4d2138f194916ef837a0b491909bcfe90188eaa
John-Mancini <mancinijohn5988@gmail.com>
2021-03-13 13:27:32 -0500

    created a variable called schedule that contains objects for every hour of an average workday

--- script.js
+++ script.js
@@ -8,3 +8,43 @@ let timer = setInterval(function () {
   let currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
   currentDay.text(currentTime);
 }, 1000);
+
+// create a variable called schedule and add object arrays that contain the times of average business hours and text containters
+let schedule = [
+  {
+    time: "9:00am",
+    text: "",
+  },
+  {
+    time: "10:00am",
+    text: "",
+  },
+  {
+    time: "11:00am",
+    text: "",
+  },
+  {
+    time: "12:00pm",
+    text: "",
+  },
+  {
+    time: "1:00pm",
+    text: "",
+  },
+  {
+    time: "2:00pm",
+    text: "",
+  },
+  {
+    time: "3:00pm",
+    text: "",
+  },
+  {
+    time: "4:00pm",
+    text: "",
+  },
+  {
+    time: "5:00pm",
+    text: "",
+  },
+];
