b512db8597706c861983b10d1ee2ae4cede3970b
John-Mancini <mancinijohn5988@gmail.com>
2021-03-13 13:37:41 -0500

    added two functions and a for loop so that each row is now on the page and is color coded to represent wether the timeslot is in the past present or future

--- script.js
+++ script.js
@@ -48,3 +48,42 @@ let schedule = [
     text: "",
   },
 ];
+
+// created a function that uses js to manipulate the dom and insert a row that contains the time text and save button and use jquery to append everything to the same line.
+function appendRow(time, text) {
+  let className = determineClassName(time);
+  let row = $('<div class="row time-block">');
+  $('<div class="hour"></div>').text(time).appendTo(row);
+  $('<textarea class=""></textarea>')
+    .val(text)
+    .addClass(className)
+    .data("time", time)
+    .appendTo(row);
+  $('<div class="saveBtn"><i class="far fa-save"></i></div>')
+    .data("time", time)
+    .appendTo(row);
+  $(row).appendTo(container);
+}
+
+//created a fuction that uses the current time of the day and compares it to the time row on the schedule that adds color codes from css to indicate wether the time is in the past present or future
+function determineClassName(time) {
+  let currentTime = moment();
+  let scheduleTime = moment(time, "h:mma");
+  let currentHour = Number(currentTime.format("HH"));
+  let scheduleHour = Number(scheduleTime.format("HH"));
+  console.log(currentHour, scheduleHour);
+  if (currentHour > scheduleHour) {
+    return "past";
+  } else if (currentHour === scheduleHour) {
+    return "present";
+  } else {
+    return "future";
+  }
+}
+
+//created a for loop so that the function append row is triggered for each object in the schedule array
+for (let i = 0; i < schedule.length; i++) {
+  const timeSlot = schedule[i];
+  console.log(timeSlot.time, timeSlot.text);
+  appendRow(timeSlot.time, timeSlot.text);
+}
